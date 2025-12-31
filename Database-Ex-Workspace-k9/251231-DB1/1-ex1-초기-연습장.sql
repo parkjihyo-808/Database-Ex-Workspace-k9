dbe6a9ce4def6e7814d6005761bd16065ec73089
parkjihyo <wlgy5678@gmail.com>
2025-12-31 16:01:12 +0900

    수정2

--- Database-Ex-Workspace-k9/251231-DB1/1-ex1-초기-연습장.sql
+++ Database-Ex-Workspace-k9/251231-DB1/1-ex1-초기-연습장.sql
@@ -5,3 +5,40 @@ select * from emp;
 
 -- 현재 시간 조회
 select sysdate from dual;
+
+-- ① 셀렉션(Selection) – 행 중심 조회
+-- 특정 조건을 만족하는 **행(Row)**만 조회
+-- sql
+-- SELECT *: 해당 테이블의 모든 칼럼을 조회
+--FROM EMP: 조회할 테이블 명
+-- WHERE 컬럼명 = 값;: 조건을 만족하는 문구
+-- 예시) WHERE JOB = 'MANAGER';
+-- 해석: emp 테이블에서 조건 job 컬럼의 값이  manager인 사원을 찾음
+-- 찾을 때 보고 싶은 열은 모든 컬럼
+SELECT * FROM EMP
+WHERE JOB = 'MANAGER';
+
+-- ② 프로젝션(Projection) - 열 중심 조회
+-- 특정 **열(Column)**만 선택하여 조회
+-- sql
+-- 보고 싶은 열에 ENAME 사원 이름, SAL 급여
+SELECT ENAME, SAL FROM EMP;
+-- 보고 싶은 열? 모든 열
+SELECT * FROM EMP;
+
+-- ③ 셀렉션 + 프로젝션 함께 사용
+-- sql
+-- 보고 싶은 열: ENAME 사원 이름, SAL 급여
+-- 조건: SAL > 2000 사원민 조회
+SELECT ENAME, SAL FROM EMP
+WHERE SAL > 2000;
+
+-- 실습 1
+--1) 급여가 3000 이상인 직원만 조회
+SELECT * FROM EMP
+WHERE SAL > 3000;
+--2) MP 테이블에서 이름(ENAME), 급여(SAL), 부서번호(DEPTNO)만 조회
+SELECT ENAME, SAL, DEPTNO FROM EMP;
+--3) 20번 부서의 사원 이름과 급여를 조회
+SELECT ENAME, SAL FROM EMP
+WHERE DEPTNO = 20;
